d9c49fce7890d0d925a939f0a845c327566adb7a
Matthew Harkey <matthewharkey@matthews-macbook-air-65.local>
2019-03-13 11:19:45 -0500

    Added segue to LocationDetailsViewController

---
+++ 'Za Hunter/LocationDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LocationDetailsViewController.swift
+//  'Za Hunter
+//
+//  Created by Matthew Harkey on 3/12/19.
+//  Copyright © 2019 Matthew Harkey. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
